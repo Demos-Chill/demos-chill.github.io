951ec9271150a65874861db36f9b2c525b3319ea
kathy <visualkathy@gmail.com>
2023-07-09 13:11:55 -0700

    Have images cycle through

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const hoverLink = document.querySelector('.hover-link');
 const hoverImage = document.querySelector('.hover-image');
 
-const images = ["https://pbs.twimg.com/media/F0YTB08aQAAzi6X?format=jpg", "https://pbs.twimg.com/media/F0YS4-IaMAAmOUq?format=jpg", "https://pbs.twimg.com/media/F0YS91nakAA3Vy2?format=jpg"];
+const imageUrls = [
+  "https://pbs.twimg.com/media/Ft69ztLaMAAKwPv?format=jpg",
+  "https://pbs.twimg.com/media/F0YTB08aQAAzi6X?format=jpg",
+  "https://pbs.twimg.com/media/F0YS4-IaMAAmOUq?format=jpg",
+  "https://pbs.twimg.com/media/F0YS91nakAA3Vy2?format=jpg"
+];
 let currentIndex = 0;
 let intervalId;
 
@@ -16,6 +21,9 @@ hoverLink.addEventListener('mouseout', () => {
 });
 
 function cycleImages() {
-  hoverImage.src = images[currentIndex];
-  currentIndex = (currentIndex + 1) % images.length;
+  hoverImage.src = imageUrls[currentIndex];
+  currentIndex = (currentIndex + 1) % imageUrls.length;
 }
+
+// Set initial image
+hoverImage.src = imageUrls[0];
